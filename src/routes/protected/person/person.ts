4ede6991ab6ec2793c446a4e93820e1e6c3c88b1
mfjkri <muhammadfikri6844@gmail.com>
2023-07-29 13:38:11 +0800

    Add readPerson route

--- src/routes/protected/person/person.ts
+++ src/routes/protected/person/person.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import CreatePersonRouter from "./createPerson";
+import ReadPersonRouter from "./readPerson";
 import UpdatePersonRouter from "./updatePerson";
 import DeletePersonRouter from "./deletePerson";
 
@@ -9,6 +10,7 @@ const router: Router = Router();
 router.use(
   "/person",
   CreatePersonRouter,
+  ReadPersonRouter,
   UpdatePersonRouter,
   DeletePersonRouter
 );
