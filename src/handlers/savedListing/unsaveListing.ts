7a7db939c912a6cd30412b727a858024f1338bac
mfjkri <muhammadfikri6844@gmail.com>
2023-07-30 14:40:43 +0800

    Add unsaveListing route

---
+++ src/handlers/savedListing/unsaveListing.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+
+import SavedListing from "../../models/savedListing";
+import User from "../../models/user";
+import { UnsaveListingParams } from "../../params/savedListing/saveListing";
+
+const SUCCESS_UNSAVED_LISTING = "Unsaved listing successfully";
+
+const ERROR_FAILED_TO_UNSAVE_LISTING = "Failed to unsave listing";
+
+export default async function handleUnsaveListing(
+  req: Request,
+  res: Response,
+  params: UnsaveListingParams
+) {
+  try {
+    const user: User = req.body.user;
+    const savedListings = await SavedListing.findAll({
+      where: { userId: user.id, listingId: params.id },
+    });
+
+    if (savedListings) {
+      if (savedListings.length > 1) {
+        for (let i = 1; i < savedListings.length; i++) {
+          await savedListings[i].destroy();
+        }
+      }
+
+      savedListings[0].isSaved = false;
+      await savedListings[0].save();
+    }
+
+    res.status(201).json({
+      message: SUCCESS_UNSAVED_LISTING,
+    });
+  } catch (error) {
+    res.status(500).json({ message: ERROR_FAILED_TO_UNSAVE_LISTING, error });
+  }
+}
--- src/routes/protected/savedListing/savedListing.ts
+++ src/routes/protected/savedListing/savedListing.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import SaveListingRouter from "./saveListing";
+import UnsaveListingRouter from "./unsaveListing";
 
 const router: Router = Router();
 
-router.use("/savedListing", SaveListingRouter);
+router.use("/savedListing", SaveListingRouter, UnsaveListingRouter);
 
 export default router;
