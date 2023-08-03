be7d2fc0ec253cbb5f5d989c16944607c904519d
mfjkri <muhammadfikri6844@gmail.com>
2023-08-03 19:21:45 +0800

    Add updateAvatar route

--- src/routes/protected/auth/auth.ts
+++ src/routes/protected/auth/auth.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 
 import GetUserRouter from "./getUser";
+import UpdateAvatarRouter from "./updateAvatar";
 import UpdatePasswordRouter from "./updatePassword";
 import UpdateUserRouter from "./updateUser";
 
 const router: Router = Router();
 
-router.use("/auth", GetUserRouter, UpdatePasswordRouter, UpdateUserRouter);
+router.use(
+  "/auth",
+  GetUserRouter,
+  UpdateAvatarRouter,
+  UpdatePasswordRouter,
+  UpdateUserRouter
+);
 
 export default router;
