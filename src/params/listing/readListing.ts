87b698fb7026d9d0810eeaf7c80a2f30d40989fb
mfjkri <muhammadfikri6844@gmail.com>
2023-08-07 01:34:24 +0800

    Remove id from request body for getListing

--- src/params/listing/readListing.ts
+++ src/params/listing/readListing.ts
@@ -1,14 +1,12 @@
 import { castParams } from "../params";
 
-export interface ReadListingParams {
-  id: number;
-}
+export interface ReadListingParams {}
 
 export function parseParams(json: any): ReadListingParams | undefined {
   return castParams<ReadListingParams>(json, typeMap);
 }
 
 const typeMap: any = {
-  props: [{ json: "id", js: "id", typ: 0 }],
+  props: [],
   additional: false,
 };
