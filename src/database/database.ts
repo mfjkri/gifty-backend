85c34dcd1e2a0f874fbe0aba9481d83919f79485
mfjkri <muhammadfikri6844@gmail.com>
2023-08-07 00:55:11 +0800

    Add backup database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,30 +1,42 @@
+import { Client } from "pg";
 import { Sequelize } from "sequelize";
+
+import pgtools from "pgtools";
+
 import { getConfig } from "../config/config";
 
-let db: Sequelize;
+const connectedDBs: Map<string, Sequelize> = new Map();
 
-export async function connectDB() {
+export async function connectDB(dbName?: string) {
   const config = getConfig();
-  db = connect({
+  const name = dbName || config.DBName;
+  const db = connect({
     Hostname: config.DBHostname,
     Port: config.DBPort,
-    Name: config.DBName,
+    Name: name,
     User: config.DBUsername,
     Password: config.DBPassword,
   });
 
   try {
     await db.authenticate();
-    console.log("Connection has been established successfully.");
+    connectedDBs.set(name, db);
+    console.log(`Connection to ${name} DB has been established successfully.`);
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error(`Unable to connect to ${name} DB:`, error);
   }
 
   return db;
 }
 
-export function getDB() {
-  return db;
+export function getDB(dbName?: string) {
+  const config = getConfig();
+  dbName = dbName || config.DBName;
+  const targetDb = connectedDBs.get(dbName);
+  if (connectedDBs.size === 0 || !targetDb) {
+    throw new Error("Database not connected");
+  }
+  return targetDb;
 }
 
 type Config = {
@@ -37,12 +49,56 @@ type Config = {
 
 function connect(config: Config) {
   const { Hostname, Port, Name, User, Password } = config;
-  const sequelize = new Sequelize(Name, User, Password, {
+  return new Sequelize(Name, User, Password, {
     host: Hostname,
     port: Port,
     dialect: "postgres",
   });
-  return sequelize;
+}
+export async function createBackupDB() {
+  const config = getConfig();
+  const backupDBName = "gifty_backup";
+
+  try {
+    const client = new Client({
+      user: config.DBUsername,
+      password: config.DBPassword,
+      host: config.DBHostname,
+      port: config.DBPort,
+      database: "postgres",
+    });
+
+    await client.connect();
+    const res = await client.query(
+      `SELECT 1 FROM pg_database WHERE datname = $1`,
+      [backupDBName]
+    );
+    await client.end();
+
+    if (res.rows.length === 0) {
+      await pgtools.createdb(
+        {
+          user: config.DBUsername,
+          password: config.DBPassword,
+          host: config.DBHostname,
+          port: config.DBPort,
+        },
+        backupDBName
+      );
+      const backupDB = await connectDB(backupDBName);
+      await backupDB.authenticate();
+      console.log(`Created backup database: ${backupDBName}`);
+      return backupDB;
+    } else {
+      const backupDB = await connectDB(backupDBName);
+      await backupDB.authenticate();
+      console.log(`Backup database already exists: ${backupDBName}`);
+      return backupDB;
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 export default getDB;
