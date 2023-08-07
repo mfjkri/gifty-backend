36b47090c02ad020017ea82c0298595a163b174a
mfjkri <muhammadfikri6844@gmail.com>
2023-08-07 23:30:13 +0800

    Update listPerson and remove readPerson

--- src/handlers/person/listPerson.ts
+++ src/handlers/person/listPerson.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Op, OrderItem } from "sequelize";
 
 import Person from "../../models/person";
 import User from "../../models/user";
@@ -15,7 +16,19 @@ export default async function handleListPerson(
 ) {
   try {
     const user: User = req.body.user;
-    const persons = await Person.findAll({ where: { userId: user.id } });
+
+    const whereClause: any = {};
+    const searchParam = req.params.search;
+    if (searchParam) {
+      whereClause[Op.or] = [{ name: { [Op.iLike]: `%${searchParam}%` } }];
+    }
+    const order: OrderItem[] = [];
+    order.push(["updatedAt", "DESC"]);
+
+    const persons = await Person.findAll({
+      where: [whereClause, { userId: user.id }],
+      order,
+    });
 
     res.status(201).json({ message: SUCCESS_LIST_PERSON, persons });
   } catch (error) {
--- src/routes/protected/person/listPerson.ts
+++ src/routes/protected/person/listPerson.ts
@@ -5,7 +5,7 @@ import handleListPerson from "../../../handlers/person/listPerson";
 
 const router: Router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
+router.get("/:search?", async (req: Request, res: Response) => {
   const params = parseParams(req.body);
   if (!params) {
     return res.status(400).json({ message: "Invalid params" });
