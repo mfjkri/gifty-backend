2f6051dc1c14f8f3a9fe13f6784cc85cb4a0634c
mfjkri <muhammadfikri6844@gmail.com>
2023-08-07 11:17:17 +0800

    Add listSavedListing route

--- src/routes/protected/listing/listing.ts
+++ src/routes/protected/listing/listing.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
 import ListListingRouter from "./listListing";
+import ListSavedListingRouter from "./listSavedListing";
 import ReadListingRouter from "./readListing";
 
 const router: Router = Router();
 
-router.use("/listing", ListListingRouter, ReadListingRouter);
+router.use(
+  "/listing",
+  ListListingRouter,
+  ListSavedListingRouter,
+  ReadListingRouter
+);
 
 export default router;
